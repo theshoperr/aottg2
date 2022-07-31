cecb3fabbc54ee65d4ec222e1cdf8aeaa943f496
Mateus Godinho <mat11godinho@hotmail.com>
2022-07-31 19:49:59 -0300

    Add files via upload

--- assets/js/Join-Community.js
+++ assets/js/Join-Community.js
@@ -2,7 +2,7 @@ var $Title = $('#Join')
 
 $Title.on('mouseenter', function(){
    
-    this.style.background = 'url("Red-selection-bigger.png") center / contain no-repeat';
+    this.style.background = 'url("assets/img/Red-selection-bigger.png") center / contain no-repeat';
     
 });
 
